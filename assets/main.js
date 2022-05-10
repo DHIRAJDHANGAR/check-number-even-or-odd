36ac7a3c03344722b0b15dffefc4e3cdd938446e
DHIRAJDHANGAR <dhadpe05@gmail.com>
2022-05-10 11:27:04 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,30 @@
+// program to check if the number is even or odd
+// take input from the user
+
+/*const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}*/
+
+/*let x = document.getElementById("list");
+
+for (let i=0; i<=100; i++){
+  var y = document.createElement("p");
+  if(i % 2 == 0){
+    y.innerHTML = i;
+    y.style.color = "red";
+    x.appendChild(y);
+  }
+  else {
+    y.innerHTML = i;
+    y.style.color = "green";
+    x.appendChild(y);
+  }
+}*/
